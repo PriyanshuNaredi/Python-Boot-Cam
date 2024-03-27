4338ddece79da0bca45fbbe97779b95a45e1a911
PriyanshuNaredi <en20cs301324@medicaps.ac.in>
2024-03-27 10:19:28 +0530

    Day-1

---
+++ Day-1/input.py
@@ -0,0 +1,9 @@
+# input("what is your name?")
+
+# print("Hello " + input("what is your name \n") + " ! ")
+
+# print(len(input()))
+
+name = input("what is your name ?")
+length = len(name)
+print(length)
